7c3ffd2d64e8b11956f9e411bd4f01ccd8262958
ChrisBeecham <chris.beecham@gmail.com>
2016-03-27 18:37:55 -0500

    Switched Computer
    Downloaded all files from CPanel. First day of working on home mac.

--- js/SecondRound.js
+++ js/SecondRound.js
@@ -3,50 +3,27 @@ $(document).ready(function()
     //Overview
     $('.beachalytics').click(function(){
         $('.item-name').text("BeachAlytics");
-        $('.item-description').html("A model and series of open-sourced reports to help non-technical people use data from Google Analytics."); 
+        $('.item-description').html("An open sourced series of reports, and an educational model. Designed to utilize information collected on Google Analytics to generate new leads and sales."); 
         $('.highlighted').removeClass("highlighted");
         $(this).addClass("highlighted");
     });
     $('.analytics').click(function(){
-       $('.item-name').text("Google Analytics");
+        $('.item-name').text("Google Analytics");
         $('.item-description').html("Free program that tracks and reports website traffic. Google Analytics is the most widely used web analytics service on the Internet."); 
         $('.highlighted').removeClass("highlighted");
         $(this).addClass("highlighted");
     });
-    $('.site-goal').click(function(){
-       $('.item-name').text("Website Goal");
-        $('.item-description').html("The goal of most sites is either to sell a product online, or lead generation. When "); 
+    $('.contact-us').click(function(){
         $('.highlighted').removeClass("highlighted");
-        $(this).addClass("highlighted");
-        $('.three').addClass("highlighted");
+        $('.phone-number').addClass("highlighted");
     });
-    $('.work-together').click(function(){
-       $('.item-name').text("Work Together");
-        $('.item-description').html("The goal of most sites is either to sell a product online, or lead generation. When "); 
-        $('.highlighted').removeClass("highlighted");
-        $(this).addClass("highlighted");
-    });
+
     //Incoming Channels
-    $('.direct').click(function(){
-       $('.item-name').text("Direct Traffic");
-        $('.item-description').html("Traffic that comes directly to the site. (Typed into the url bar, link from an email.)"); 
-        $('.details').html("Go<span class='glyphicon glyphicon-chevron-right'></span>");
-        $('.highlighted').removeClass("highlighted");
-        $(this).addClass("highlighted");
-    });
-    $('.email').click(function(){
-       $('.item-name').text("Email Traffic");
-        $('.item-description').html("Links from emails normally appear as <span class='direct'>Direct</span> links. Some special steps can be taken to track email, and have them appear as their own channel."); 
-        $('.details').html("Go<span class='glyphicon glyphicon-chevron-right'></span>");
-//        $('.email').css("background", "#ff9900");
-//        $('.email').css("color", "#f3f3f3");
-        $('.highlighted').removeClass("highlighted");
-        $(this).addClass("highlighted");
-    });
+    
     $('.referral').click(function(){
         $('.item-name').text("Referral Traffic");
-        $('.item-description').html("Traffic from other websites.");
-        $('.details').html("Go<span class='glyphicon glyphicon-chevron-right'></span>");
+        $('.item-description').html("Traffic that gets to your site from other sites. Referral traffic often gets divided into sub-channels; social media, organic search, paid advertisements.")
+        $('.details').html("More on Referral Links<span class='glyphicon glyphicon-chevron-right'></span>");
 //        $('.referral').css("background", "#0000ff");
 //        $('.referral').css("color", "#f3f3f3");
         $('.highlighted').removeClass("highlighted");
@@ -54,8 +31,8 @@ $(document).ready(function()
     });
     $('.search').click(function(){
        $('.item-name').text("Organic Search");
-        $('.item-description').html("Traffic from search engine results page. (Google, Yahoo)"); 
-        $('.details').html("Go<span class='glyphicon glyphicon-chevron-right'></span>");
+        $('.item-description').html("Traffic from search engine results pages, ex. Google and Yahoo. Organic Search is a subsection of <span class='referral'>Referral Traffic</span>."); 
+        $('.details').html("Best Practices<span class='glyphicon glyphicon-chevron-right'></span>");
 //        $('.search').css("background", "#51a351");
 //        $('.search').css("color", "#f3f3f3");
         $('.highlighted').removeClass("highlighted");
@@ -63,32 +40,48 @@ $(document).ready(function()
     });
     $('.ads').click(function(){
         $('.item-name').text("Paid Advertisements");
-      $('.item-description').html("Traffic that arrives to the site via an advertisements. The two most common types of ads are 'Search' ads and 'Display' ads.)"); 
-        $('.details').html("Go<span class='glyphicon glyphicon-chevron-right'></span>");
+      $('.item-description').html("Visitors that arrive to the site via an advertisements. The common types of online ads are 'Search' ads and 'Display' ads.)"); 
+        $('.details').html("Learn More<span class='glyphicon glyphicon-chevron-right'></span>");
 //        $('.ads').css("background", "#4a86e8");
 //        $('.ads').css("color", "#f3f3f3");
+        $('.highlighted').removeClass("highlighted");
+        $(this).addClass("highlighted");
+    });
+    $('.direct').click(function(){
+       $('.item-name').text("Direct Traffic");
+        $('.item-description').html("Traffic that comes directly to the site.(Typed into the url bar, link from an email.) Often times, Direct traffic consists of links that don't fit in any other category."); 
+        $('.details').html("Learn More<span class='glyphicon glyphicon-chevron-right'></span>");
+        $('.highlighted').removeClass("highlighted");
+        $(this).addClass("highlighted");
+    });
+    $('.email').click(function(){
+       $('.item-name').text("Email Traffic");
+        $('.item-description').html("Links from emails normally appear as <span class='direct'>Direct</span> links, since they don't come from another site. Special steps can be taken to track emails, figuring out which messages and lists are the most successful."); 
+        $('.details').html("Tracking Emails<span class='glyphicon glyphicon-chevron-right'></span>");
+//        $('.email').css("background", "#ff9900");
+//        $('.email').css("color", "#f3f3f3");
         $('.highlighted').removeClass("highlighted");
         $(this).addClass("highlighted");
     });
     //Target
     $('.one').click(function(){
         $('.item-name').html("Landing Pages");
-        $('.item-description').html("Landing pages are the first impression the outside world has of your website. Google uses landing pages to rate your site, determining when you will appear on different searches. Improvements made at this level can attract more visitors AND get them closer towards the end goal.");
-        $('.details').html("Go<span class='glyphicon glyphicon-chevron-right'></span>");
+        $('.item-description').html("Landing pages are the first impression the outside world has of your website. Google uses landing pages to rate your site, determining when you will appear on different searches.");
+        $('.details').html("Learn More<span class='glyphicon glyphicon-chevron-right'></span>");
         $('.highlighted').removeClass("highlighted");
         $(this).addClass("highlighted");
     });
     $('.two').click(function(){
         $('.item-name').text("Signup Pages");
-        $('.item-description').html("The signup page represents the steps a user goes through to make a purchase on the site. Improving the funnel directly leads to more sales. Important metrics 1) Click Through Rate - How many people move from one step to another. 2) Conversion Rate - Percent of total visits that make it to the confirmation page.");
-        $('.details').html("Go<span class='glyphicon glyphicon-chevron-right'></span>");
+        $('.item-description').html("Signup pages are the steps a user goes through to complete your site's goal. Improvements made to the signup pages can directly lead to new leads & sales.");
+        $('.details').html("Learn More<span class='glyphicon glyphicon-chevron-right'></span>");
         $('.highlighted').removeClass("highlighted");
         $(this).addClass("highlighted");
     });
     $('.three').click(function(){
         $('.item-name').text("Confirmation Pages");
         $('.item-description').html("The confirmation page is loaded directly after each sale, showing the customer their purchase. This page helps tie sales and revenue to marketing efforts.");
-        $('.details').html("Go<span class='glyphicon glyphicon-chevron-right'></span>");
+        $('.details').html("Learn More<span class='glyphicon glyphicon-chevron-right'></span>");
         $('.highlighted').removeClass("highlighted");
         $(this).addClass("highlighted");
     });
