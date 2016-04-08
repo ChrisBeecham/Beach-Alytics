dfe541958590c71ad92db914490fd3d38f64cc05
ChrisBeecham <chris.beecham@gmail.com>
2016-04-08 12:10:05 -0500

    Added Services Section
    -Updated HTML, CSS, JS

--- js/SecondRound.js
+++ js/SecondRound.js
@@ -2,29 +2,34 @@ $(document).ready(function()
 {
     //Overview
     $('.beachalytics').click(function(){
-        $('.definition').html("<p class='item-name'>BeachAlytics</p><p class='item-description'>An open sourced series of reports, and an educational model. Designed to utilize information collected on Google Analytics to generate new leads and sales.</p>");
+        $('.item-name').text("BeachAlytics");
+        $('.item-description').text("An open sourced series of reports, and an educational model. Designed to utilize information collected on Google Analytics to generate new leads and sales.");
         $('.highlighted').removeClass("highlighted");
         $(this).addClass("highlighted");
     });
     $('.analytics').click(function(){
-        $('.definition').html("<p class='item-name'>Google Analytics</p><p class='item-description'>Free program that tracks and reports website traffic. Google Analytics is the most widely used web analytics service on the Internet.</p>");
+        $('.item-name').text("Google Analytics");
+        $('.item-description').text("Free program that tracks and reports website traffic. Google Analytics is the most widely used web analytics service on the Internet.");
         $('.highlighted').removeClass("highlighted");
         $(this).addClass("highlighted");
     });
 
     //Incoming Channels
     $('.referral').click(function(){
-        $('.definition').html("<p class='item-name'>Referral Traffic</p><a href='#referral-table' class='btn btn-success btn-lg details pull-right'>Reporting</a><p class='item-description'>Traffic that gets to your site from other sites. Referral traffic often gets divided into sub-channels; social media, organic search, paid advertisements.</p>");
+        $('.item-name').text("Referral Traffic");
+        $('.item-description').text("Traffic that gets to your site from other sites. Referral traffic often gets divided into sub-channels; social media, organic search, paid advertisements.");
         $('.highlighted').removeClass("highlighted");
         $(this).addClass("highlighted");
     });
     $('.search').click(function(){
-        $('.definition').html("<p class='item-name'>Organic Search</p><a href='#organic-search-table' class='btn btn-success btn-lg details pull-right'>Improving Search</a><p class='item-description'>Traffic from search engine results pages, ex. Google and Yahoo. Organic Search is a subsection of <span class='referral'>Referral Traffic</span>.</p>");
+        $('.item-name').text("Organic Search");
+        $('.item-description').html("Traffic from search engine results pages, ex. Google and Yahoo. Organic Search is a subsection of <span class='referral'>Referral Traffic</span>.");
         $('.highlighted').removeClass("highlighted");
         $(this).addClass("highlighted");
     });
     $('.ads').click(function(){
-        $('.definition').html("<p class='item-name'>Paid Advertisements</p><a href='#paid-ads-table' class='btn btn-success btn-lg details pull-right'>Targeting Ads</a><p class='item-description'>Visitors that arrive to the site via an advertisements. The common types of online ads are 'Search' ads and 'Display' ads.)</p>");
+        $('.item-name').text("Paid Advertisements");
+        $('.item-description').html("Visitors that arrive to the site via an advertisements. The common types of online ads are 'Search' ads and 'Display' ads. Paid Advertisements are a subsection of <span class='referral'>Referral Traffic</span>.");
         $('.highlighted').removeClass("highlighted");
         $(this).addClass("highlighted");
     });
@@ -34,7 +39,8 @@ $(document).ready(function()
         $(this).addClass("highlighted");
     });
     $('.email').click(function(){
-        $('.definition').html("<p class='item-name'>Email Campaigns</p><a href='#email-table' class='btn btn-success btn-lg details pull-right'>Tracking Emails</a><p class='item-description'>Links from emails normally appear as <span class='direct'>Direct</span> links, since they don't come from another site. Special steps can be taken to track emails, figuring out which messages and lists are the most successful.</p>");
+        $('.item-name').text("Email Campaigns");
+        $('.item-description').html("Links from emails normally appear as <span class='direct'>Direct</span> links, since they don't come from another site. Special steps can be taken to track emails, figuring out which messages and lists are the most successful.");
         $('.highlighted').removeClass("highlighted");
         $(this).addClass("highlighted");
     });
@@ -51,7 +57,7 @@ $(document).ready(function()
         $(this).addClass("highlighted");
     });
     $('.three').click(function(){
-        $('.definition').html("<p class='item-name'>Confirmation Pages</p><p class='item-description'>The confirmation page is loaded directly after each sale, showing the customer their purchase. This page helps tie sales and revenue to marketing efforts.</p>");
+        $('.definition').html("<p class='item-name'>Confirmation Page</p><p class='item-description'>The Confirmation Page, or Thank You Page is loaded directly after each sale, showing the customer their purchase. This page helps tie sales and revenue to marketing efforts.</p>");
         $('.highlighted').removeClass("highlighted");
         $(this).addClass("highlighted");
     });
